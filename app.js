ca3fca29aa3034e2d068a19e62c94e9d7c2f3783
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2020-12-15 01:02:49 +0600

    first data serving

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+const fs = require("fs");
+
 const express = require("express");
 const app = express();
+
+const todos = JSON.parse(fs.readFileSync(`${__dirname}/data/todos.json`));
 
 const port = 3001;
 app.listen(port, () => {
   console.log(`express server runnig at http://localhost:${port}`);
 });
 app.get("/", (req, res) => {
-  res.status(200).send("i am runnig from nodemon");
+  //   res.status(200).send("i am runnig from nodemon");
+  res.status(200).json({
+    status: "success",
+    results: todos.length,
+    data: todos,
+  });
 });
