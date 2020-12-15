02cc4f16453719d4f3faeb8dfe5784a5d1daaa4c
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2020-12-15 11:27:07 +0600

    get post request done

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 import express from "express";
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import path from "path";
 
 const app = express();
 // __dirname is not defined in REPL(shell exe ) files in v-14 so user import.meta.url
 // const todos = JSON.parse(await readFile(`./data/todos.json`)); => like this relative path or
-const todos = JSON.parse(await readFile(`${process.cwd()}/data/todos.json`));
+let todos = JSON.parse(await readFile(`${process.cwd()}/data/todos.json`));
 
 const port = 3001;
 app.listen(port, () => {
   console.log(`express server runnig at http://localhost:${port}`);
 });
-app.get("/", (req, res) => {
+
+//middlewares
+
+app.use(express.json());
+
+app.get("/todos", (req, res) => {
   //   res.status(200).send("i am runnig from nodemon");
   res.status(200).json({
     status: "success",
     results: todos.length,
     data: todos,
+  });
+});
+app.post("/todos", (req, res) => {
+  console.log(req.body);
+  const newId = todos.length + 1;
+  const body = req.body;
+  const newTask = Object.assign({ id: newId }, body);
+  todos.push(newTask);
+  writeFile(
+    `${process.cwd()}/data/todos.json`,
+    JSON.stringify(todos),
+    (err) => {
+      console.log(err);
+    }
+  );
+
+  res.status(201).json({
+    id: newId,
+    message: "post success",
   });
 });
